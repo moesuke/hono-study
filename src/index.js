ae20fcf9036c7c4844b690763109be7571d92764
moesuke <info@moesuke.com>
2024-03-26 18:01:19 +0900

    ABPICN01_G002 before practice

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { Hono } = require("hono");
+const { serve } = require("@hono/node-server");
+const { logger } = require('hono/logger');
+const { html } = require('hono/html');
+
+const app = new Hono();
+app.use(logger());
+
+app.get("/", (c) => {
+  const name = c.req.query("name") ?? "ゲスト";
+  return c.html(html`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Home</title>
+      </head>
+      <body>
+      <h1>こんにちは！ ${name} さん</h1>
+        <p>
+          これは Hono のサンプルアプリケーションです。
+        </p>
+      </body>
+    </html>
+  `);
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
